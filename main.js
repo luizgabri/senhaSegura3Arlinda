4e76c2d2a1665018cfe0c853daf857d5b9ecd584
luizgabri <160794599+luizgabri@users.noreply.github.com>
2025-07-02 20:42:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const numeroSenha = document .querySelector('.parametro_senha_texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent.textContent = tamanhoSenha
+Const letrasMinusculas ='abcdefghijklmnopqrstuvwxyz'
+Const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const numeros ='0123456789'
